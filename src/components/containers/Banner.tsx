1680869893cf10a2506afa212ed8c86bf5a2647f
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-01-23 19:19:12 +0300

    Fix: Banner delay & layout

--- src/components/containers/Banner.tsx
+++ src/components/containers/Banner.tsx
@@ -1,54 +1,43 @@
-import React, {FC} from 'react'
-import {Swiper, SwiperSlide} from 'swiper/react'
-import {Autoplay, Pagination} from 'swiper'
-import {useGetBannerQuery} from '../../services/RTK/bannerApi'
+import React, { FC } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, Pagination } from 'swiper'
+import { useGetBannerQuery } from '../../services/RTK/bannerApi'
 import Loader from '../utils/Loader'
-import {checkPhotoPath} from '../../helpers/photoLoader'
+import { checkPhotoPath } from '../../helpers/photoLoader'
 
 type Props = {
     swiperDelay?: number
 }
 
-const BannerContainer: FC<Props> = ({swiperDelay}) => {
-    const {data, error, isLoading} = useGetBannerQuery()
+const BannerContainer: FC<Props> = ({ swiperDelay }) => {
+    const { data, error, isLoading } = useGetBannerQuery()
+
+    if (!swiperDelay) return null
 
     return (
         <Swiper
             className="swiper-1"
             modules={[Pagination, Autoplay]}
             slidesPerView={1}
-            pagination={{clickable: true}}
-            autoplay={{delay: swiperDelay && swiperDelay * 1000}}
+            pagination={{ clickable: true }}
+            autoplay={{ delay: swiperDelay && swiperDelay * 1000 }}
         >
             {!isLoading ? (
                 data ? (
                     data?.body?.map((item) => (
                         <SwiperSlide key={item?.id}>
-                            {item?.link ? (
-                                <a href={item?.link}>
-                                    <img src={checkPhotoPath(item?.image)} alt={item?.title} />
-                                    <div className="container white">
-                                        <div className="row">
-                                            <div className="col-md-9 col-lg-7">
-                                                <h2>{item?.title}</h2>
-                                                <h5 dangerouslySetInnerHTML={{__html: item?.description}} />
-                                            </div>
+                            <>
+                                <img src={checkPhotoPath(item?.image)} alt={item?.title} />
+                                <div className="container white">
+                                    <div className="row">
+                                        <div className="col-md-9 col-lg-7">
+                                            <h2>{item?.title}</h2>
+                                            <h5 dangerouslySetInnerHTML={{ __html: item?.description }} />
                                         </div>
                                     </div>
-                                </a>
-                            ) : (
-                                <>
-                                    <img src={checkPhotoPath(item?.image)} alt={item?.title} />
-                                    <div className="container white">
-                                        <div className="row">
-                                            <div className="col-md-9 col-lg-7">
-                                                <h2>{item?.title}</h2>
-                                                <h5 dangerouslySetInnerHTML={{__html: item?.description}} />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </>
-                            )}
+                                </div>
+                                {item?.link && <a href={item?.link} />}
+                            </>
                         </SwiperSlide>
                     ))
                 ) : (
