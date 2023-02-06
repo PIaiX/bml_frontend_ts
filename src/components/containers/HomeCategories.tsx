b4277a8d6c5825c3cff7340fd65e2a4432ad61f6
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-02-06 15:09:49 +0300

    Fix: Ads with the same height on home in mobile

--- src/components/containers/HomeCategories.tsx
+++ src/components/containers/HomeCategories.tsx
@@ -1,8 +1,8 @@
-import React, {FC} from 'react'
+import React, { FC } from 'react'
 import AdvPreview from '../AdvPreview'
 import Loader from '../utils/Loader'
-import {NavLink} from 'react-router-dom'
-import {IOffersItem, IOffersMeta} from '../../types/offers'
+import { NavLink } from 'react-router-dom'
+import { IOffersItem, IOffersMeta } from '../../types/offers'
 
 type Props = {
     isLoadingInvestors?: boolean
@@ -37,7 +37,7 @@ const HomeCategoriesContainer: FC<Props> = ({
                     {isLoadingInvestors ? (
                         investors?.length ? (
                             investors?.map((item) => (
-                                <div className="col-6 col-md-4 col-lg-3" key={item.id}>
+                                <div className="col-6 col-md-4 col-lg-3 position-relative" key={item.id}>
                                     <AdvPreview
                                         id={item.id}
                                         image={item.image}
@@ -65,7 +65,7 @@ const HomeCategoriesContainer: FC<Props> = ({
                     {isLoadingSuggestionsInvestors ? (
                         suggestionsInvestors?.length ? (
                             suggestionsInvestors?.map((item) => (
-                                <div className="col-6 col-md-4 col-lg-3" key={item.id}>
+                                <div className="col-6 col-md-4 col-lg-3 position-relative" key={item.id}>
                                     <AdvPreview
                                         id={item.id}
                                         image={item.image}
@@ -93,7 +93,7 @@ const HomeCategoriesContainer: FC<Props> = ({
                     {isLoadingBusinessPartners ? (
                         businessPartners?.length ? (
                             businessPartners?.map((item) => (
-                                <div className="col-6 col-md-4 col-lg-3" key={item.id}>
+                                <div className="col-6 col-md-4 col-lg-3 position-relative" key={item.id}>
                                     <AdvPreview
                                         id={item.id}
                                         image={item.image}
@@ -121,7 +121,7 @@ const HomeCategoriesContainer: FC<Props> = ({
                     {isLoadingSaleBusiness ? (
                         saleBusiness?.length ? (
                             saleBusiness?.map((item) => (
-                                <div className="col-6 col-md-4 col-lg-3" key={item.id}>
+                                <div className="col-6 col-md-4 col-lg-3 position-relative" key={item.id}>
                                     <AdvPreview
                                         id={item.id}
                                         image={item.image}
@@ -149,7 +149,7 @@ const HomeCategoriesContainer: FC<Props> = ({
                     {isLoadingFranchise ? (
                         franchise?.length ? (
                             franchise?.map((item) => (
-                                <div className="col-6 col-md-4 col-lg-3" key={item.id}>
+                                <div className="col-6 col-md-4 col-lg-3 position-relative" key={item.id}>
                                     <AdvPreview
                                         id={item.id}
                                         image={item.image}
